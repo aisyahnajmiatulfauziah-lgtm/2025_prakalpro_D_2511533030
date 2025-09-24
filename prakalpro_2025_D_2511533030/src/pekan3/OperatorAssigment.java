cc079f519d019cb505b44269fdcadb76ad8ec3d2
ASUS <ASUS@DESKTOP-QJED8U8>
2025-09-24 16:06:17 +0700

    Praktikum pekan3

--- prakalpro_2025_D_2511533030/src/pekan3/OperatorAssigment.java
+++ prakalpro_2025_D_2511533030/src/pekan3/OperatorAssigment.java
@@ -1,5 +1,32 @@
 package pekan3;
 
+import java.util.Scanner;
 public class OperatorAssigment {
-
+	public static void main(String[]  args)  {
+		int A1;
+		   int A2;
+		   Scanner keyboard = new Scanner(System.in);
+		   System.out.print("Input angka-1: ");
+		   A1 = keyboard.nextInt();
+		   System.out.print("Input angka-2: ");
+		   A2 = keyboard.nextInt();
+		   keyboard.close();//input 5
+		   System.out.println("Assigment penambahan ");
+		   A1 += A2;// penambahan, sekarang A1 = 15
+		   System.out.println("Penambahan : " + A1);
+		   System.out.println("Assigment pengurangan ");
+		   A1 -= A2;// pengurangan, sekarang A1 = 10
+		   System.out.println("Pengurangan : " + A1);
+		   System.out.println("Assigment perkalian ");
+		   A1 *= A2;// perkalian, sekarang A1 = 50
+		   System.out.println("Perkalian : " + A1);
+		   System.out.println("Assigment hasil bagi");
+		   A1 /= A2;// hasil bagi, sekarang A1 = 10
+		   System.out.println("Pembagian : " + A1);
+		   System.out.println("Assigment sisa bagi ");
+		   A1 %= A2;// sisa bagi, sekarang A1 = 0
+		   System.out.println("Sisa Bagi: " + A1);
+		   
+	
+	}
 }
